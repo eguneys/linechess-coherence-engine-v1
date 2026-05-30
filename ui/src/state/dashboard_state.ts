bd290591c069c399d8c8ed6ae8e99800587aff71
eguneys <eguneys@gmail.com>
2026-05-31 00:35:12 +0300

    login stuff

--- ui/src/state/dashboard_state.ts
+++ ui/src/state/dashboard_state.ts
@@ -19,6 +19,7 @@ export type DashboardStore = [DashboardState, DashboardActions]
 
 export type DashboardPersistedStore = {
     logged_in_profile?: LoggedInProfile
+    redirecting_login: boolean
 }
 
 
@@ -27,15 +28,17 @@ export function make_dashboard(): DashboardStore {
     let $api = create_api_agent()
 
     let [store, set_store] = makePersisted(createStore<DashboardPersistedStore>({
-        logged_in_profile: undefined
+        logged_in_profile: undefined,
+        redirecting_login: false
     }), { name: '.linechess.dashboardstore.v4'})
 
-    const [fetch_login, set_fetch_login] = createSignal(true)
+    const [fetch_login, set_fetch_login] = createSignal(store.logged_in_profile !== undefined || store.redirecting_login)
     const logged_in_profile = createAsync(async () => {
         let should_fetch = fetch_login()
         set_fetch_login(false)
 
         if (should_fetch) {
+            set_store('redirecting_login', false)
             try {
             let res = await $api.fetch_lichess_username()
                 if (res.username) {
@@ -60,6 +63,7 @@ export function make_dashboard(): DashboardStore {
 
     let actions = {
         async login_with_lichess() {
+            set_store('redirecting_login', true)
             await $api.login_with_lichess()
         },
         async logout() {
