3cd28a1c7966e01fbd18cc5a6238146d42f36acd
eguneys <eguneys@users.noreply.github.com>
2026-06-01 14:25:54 +0000

    deploy: ffd99d3d7733d5fe313fb00a8c05a20049400341

--- assets/NotFound-OHvaZNsE.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./index-DHd6xIPs.js";/* empty css             */var t=e(`<main class=not-found><div class=box><div class=header>404 · Not Found</div><div class=body><h2>404</h2><span>·</span><p>Not Found`);function n(){return t()}export{n as default};
