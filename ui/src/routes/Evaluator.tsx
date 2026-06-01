971e0ffc4a51a1cca79251028c068529850a82da
eguneys <eguneys@gmail.com>
2026-06-01 14:20:16 +0300

    tweaks

--- ui/src/routes/Evaluator.tsx
+++ ui/src/routes/Evaluator.tsx
@@ -177,10 +177,10 @@ function Assesment() {
         <div class='right'>
             <div class='title'>Profile Assesment: <A href={`https://lichess.org/@/${state.username}`} target="_blank">{state.username}</A></div>
             <div class='bars'>
-                <OneBarWithLabel label={`bullet (${state.params.Pb.Gtarget})`} progress={state.fitnessScore!.T_b * 100}/>
-                <OneBarWithLabel label={`blitz (${state.params.Pz.Gtarget})`} progress={state.fitnessScore!.T_z * 100}/>
-                <OneBarWithLabel label={`rapid (${state.params.Pr.Gtarget})`} progress={state.fitnessScore!.T_r * 100}/>
-                <OneBarWithLabel label={`classical (${state.params.Pc.Gtarget})`} progress={state.fitnessScore!.T_c * 100}/>
+                <OneBarWithLabel label={`bullet (${state.fitnessScore!.Nb.length}/${state.params.Pb.Gtarget})`} progress={state.fitnessScore!.T_b * 100}/>
+                <OneBarWithLabel label={`blitz (${state.fitnessScore!.Nz.length}/${state.params.Pz.Gtarget})`} progress={state.fitnessScore!.T_z * 100}/>
+                <OneBarWithLabel label={`rapid (${state.fitnessScore!.Nr.length}/${state.params.Pr.Gtarget})`} progress={state.fitnessScore!.T_r * 100}/>
+                <OneBarWithLabel label={`classical (${state.fitnessScore!.Nc.length}/${state.params.Pc.Gtarget})`} progress={state.fitnessScore!.T_c * 100}/>
             </div>
         </div>
             </div>
